52715a0e0dcea5212f4e28f9c407af1a07f2f956
Dougal Wallace <dougal.wallace@gmail.com>
2023-08-03 16:04:05 +0100

    pretty good stage

--- Blackjack/blackjack.js
+++ Blackjack/blackjack.js
@@ -3,14 +3,10 @@ $(document).ready(function(){ // start script only after page load
 //setup starts here
  // initialize our ugly deck object as a global variable from the function down below out of sight
 let playerArray = []; //initialize the array we'll hold player cards in
-//let playerScore = addScore(playerArray);
 let dealerArray = []; //same for the dealer's array
-//let dealerScore = addScore(dealerArray);
+let dealerCounter = 1; //variable we use to animate the dealer's turn when standing
 wallet = 610; //starting cash
-let $outputText = $('#text'); 
-let $outputLowerText = $('#lowerText');
-// we need this here as lots of stuff references it
-
+let cardStyle = '<span style=background-color:white;color:black>' // there MUST be a better way to do this
 
 const blackjackTable = 
     {
@@ -24,103 +20,78 @@ var room = blackjackTable;
 initialize();
 
 function initialize (){
+    $('.story').remove();
     deckInitialize();
     playerArray = [];
     dealerArray = [];
     drawCard(dealerArray, 1);
     drawCard(playerArray, 2);
+    dealerCounter = 1;
+    addStoryLine(room.story, 2000);
+    addStoryLine('the dealer draws the ' +cardStyle + dealerArray[0].face + ' of ' + dealerArray[0].suit, 2000);
+    addStoryLine(`she deals you the ` + cardStyle+ playerArray[0].face + ' of ' + playerArray[0].suit + '</span>', 3000);
+    addStoryLine('and the ' + cardStyle + playerArray[1].face + " of " + playerArray[1].suit, 3500);
 
-    addStoryLine(room.story, 1000);
-    addStoryLine('the dealer draws the ' + dealerArray[0].face + ' of ' + dealerArray[0].suit, 2000);
-    addStoryLine(`the dealer slides the ` + playerArray[0].face + ' of ' + playerArray[0].suit + ' across to you', 3000);
-    addStoryLine('and the ' + playerArray[1].face + ' of ' + playerArray[1].suit, 3500);
-
-    addStoryLine(room.options, 4000);
+    //addStoryLine(' your score is '+ cardStyle + addScore(playerArray) + '</span>' + " against the dealer's " + cardStyle + addScore(dealerArray), 4000);
+    reportScore(4000);
+    addStoryLine(room.options, 4500);
  
-
-   
-    let $playerHand = $('#playerHand');
-    $playerHand.text('your hand')
-    
-    // dealer's hand
-    let $dealerHand = $('#dealerHand');
-    $dealerHand.text("dealer's hand")
-    for (let i=0; i < dealerArray.length; i++){
-        $('#dealerHand').append('<br>the ' + dealerArray[i].face + ' of ' + dealerArray[i].suit);
-    }
-    $('#dealerScore').text("dealer's score is " + addScore(dealerArray))
-    
-    // doing player's hand here
-    for (let i=0; i < playerArray.length; i++){
-        $('#playerHand').append('<br>the ' + playerArray[i].face + ' of ' + playerArray[i].suit);
-    }
-    $('#playerScore').text(' your score is ' + addScore(playerArray))
-
-
-/*
-addStoryLine('hi')
-
-*/
-
 }
 
 
-
-$('#textInput').on('submit', function (event) {
+$('#textInput').on('submit', function (event) { //huge text input and logic system. massive kludge. don't look too closely.
     event.preventDefault();    
     let $input = $('#textBox');
     let input = $input.val();
     input = input.toLowerCase(); // sanitise case
     if(input == 'hit'){
         drawCard(playerArray,1);
-        //addScore(playerArray);
-        $('#playerHand').append('<br>the ' + playerArray[playerArray.length -1].face + ' of ' + playerArray[playerArray.length -1].suit);
-        $('#playerScore').text(' your score is ' + addScore(playerArray))
-
-/*      commenting this out as I misunderstood the rules
-        drawCard(dealerArray,1);
-        $('#dealerHand').append('<br>the ' + dealerArray[dealerArray.length -1].face + ' of ' + dealerArray[dealerArray.length -1].suit);
-        $('#dealerScore').text(" dealer's score is " + addScore(dealerArray))
-*/
+        $('.story').last().remove();
+        $('.story').last().remove();
+        addStoryLine('the dealer slides the ' +cardStyle + playerArray[playerArray.length -1].face + ' of ' + playerArray[playerArray.length -1].suit + '</span> across to you', 1000);
+        reportScore(1000);
+        addStoryLine(room.options)
     } 
     else if(input== 'stand') {
         while (addScore(dealerArray) < 16){
         drawCard(dealerArray,1);
-        $('#dealerHand').append('<br>the ' + dealerArray[dealerArray.length -1].face + ' of ' + dealerArray[dealerArray.length -1].suit);
-        $('#dealerScore').text(" dealer's score is " + addScore(dealerArray))
+        addStoryLine('dealer draws', dealerCounter*1500);
+        reportScore(dealerCounter*1500);
+        dealerCounter++;
+        //$('#dealerHand').append('<br>the ' + dealerArray[dealerArray.length -1].face + ' of ' + dealerArray[dealerArray.length -1].suit);
+       // $('#dealerScore').text(" dealer's score is " + addScore(dealerArray))
         }
     } else if(input== 'x'){
-        $('.story').remove();
         initialize();
     }
-
-
     else {
-        $outputLowerText.text('invalid command, please hit or stand');
+        addStoryLine('invalid command, please hit or stand');
     }
 
     if (addScore(playerArray) == 21 && addScore(dealerArray) != 21) {
-        $outputLowerText.text('you win');
+        //$('.story').last().remove();
+        addStoryLine('you win',1000);
     } else if (addScore(playerArray) < 21 && addScore(dealerArray) > 21) {
-        $outputLowerText.text('you win');
+       // $('.story').last().remove();
+        addStoryLine('you win',1000);
     } else if (addScore(playerArray) > 21) {
-        $outputLowerText.text('you lose, press x to restart');
-    }
+       // $('.story').last().remove();
+        addStoryLine('you lose, press x to restart',1000);
+    } 
 
     $('#textBox').val('');
 
 })
 
-//$('#lowerText').append('stuff'+ '<br>');
-/* deprecated function for doing options in rooms
-if (room.allowedOptions.indexOf(input) != -1)
-{$outputText.text(room[input]);}
-*/
 
 function addStoryLine(story, delay) { // helped along by a code example at https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_html_append2
     var txt = $("<p class='story'>"+story+"</p>")
     $("#main").append(txt);
     $('.story').animate({opacity:1}, delay);
+}
+
+function reportScore(delay) {
+    addStoryLine(' your score is '+ cardStyle + addScore(playerArray) + '</span>' + " against the dealer's " + cardStyle + addScore(dealerArray), delay);
 }
 
 function getRandomInt(max) {  //from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -130,11 +101,14 @@ function getRandomInt(max) {  //from https://developer.mozilla.org/en-US/docs/We
 // moves 'num' cards to an array 'array' and splices them out of the deck. this still needs an interrupt to implement ace logic
 function drawCard(array, num) { 
     for (i=1; i<=num; i++) {
+        if (deck.length == 0) { // reshuffle if out of cards
+            addStoryLine('The dealer notices the deck of cards is empty and smoothy opens another one')
+            deckInitialize();
+        }
         let drawnCard = getRandomInt(deck.length);
         //console.log(drawnCard);
         array.push(deck[drawnCard]);
         deck.splice(drawnCard,1);
-
     }
 }
 
@@ -478,3 +452,20 @@ const lobby =
 
 };
 });
+
+//$('#lowerText').append('stuff'+ '<br>');
+/* deprecated function for doing options in rooms
+if (room.allowedOptions.indexOf(input) != -1)
+{$outputText.text(room[input]);}
+*/
+
+ // addStoryLine("<p style='background-color:orange; display: inline-block'>GeeksforGeeks</p>")
+
+function addCard(card) { // trying to get black card on white bg working
+    var txt = `<p class=redCard>${card}</p>`
+    return txt;
+    //$("#main").append(txt);
+    //$('.story').animate({opacity:1}, delay);
+}
+
+
